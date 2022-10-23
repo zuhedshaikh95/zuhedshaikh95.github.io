f6cd6b7328a9b52421d7efcae509ac1ade21eea8
zuhedshaikh95 <zuhed95@gmail.com>
2022-10-23 11:43:01 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Zuhed Shaikh</h1>
+      <p>Full Stack Web Developer</p>
+      <em>React App</em>
+    </div>
+  );
+}
+
+export default App;
