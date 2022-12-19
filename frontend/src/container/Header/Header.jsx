4cc987bb624b4d91bda2cb728a89f3190df2ac64
zuhedshaikh95 <zuhed95@gmail.com>
2022-12-19 16:28:42 +0530

    Header update

--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -1,76 +1,43 @@
 import { motion } from "framer-motion";
 
-import { AppWrap } from "../../wrapper";
+
 import { images } from "../../constants";
+import { AppWrap } from "../../wrapper";
+import Typewriter from "typewriter-effect";
+
+import {Heading, Text, Container, HStack} from "@chakra-ui/react";
 import "./Header.scss";
-
-const scaleVariants = {
-  whileInView: {
-    scale: [0, 1],
-    opacity: [0, 1],
-    transition: {
-      duration: 1,
-      ease: "easeInOut",
-    },
-  },
-};
-
-
-const skillsCircle = [images.redux , images.react, images.mongodb];
 
 const Header = () => {
   return (
     <div className="app__header app__flex">
-      <motion.div
-        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
-        transition={{ duration: 0.5 }}
-        className="app__header-info"
-      >
-        <div className="app__header-badge">
-          <div className="badge-cmp app__flex">
-            <span>👋</span>
-            <div style={{ marginLeft: 20 }}>
-              <p className="p-text">Hello, I am</p>
-              <h1 className="head-text">Zuhed</h1>
-              <h1 className="head-text">Shaikh</h1>
-            </div>
-          </div>
-
-          <div className="tag-cmp app__flex">
-            <p className="p-text">Full Stack Web Developer</p>
-            <p className="p-text">Masai School</p>
-          </div>
-        </div>
-      </motion.div>
-
-      <motion.div
-        whileInView={{ opacity: [0, 1] }}
-        transition={{ duration: 0.5, delayChildren: 0.5 }}
-        className="app__header-img"
-      >
-        <img src={images.zuhed} alt="profile_bg" />
-        <motion.img
-          whileInView={{ scale: [0, 1] }}
-          transition={{ duration: 1, ease: "easeInOut" }}
-          src={images.circle}
-          alt="profile_circle"
-          className="overlay_circle"
-        />
-      </motion.div>
- 
-      <motion.div
-        variant={scaleVariants}
-        whileInView={scaleVariants.whileInView}
-        className="app__header-circles"
-      >
-        {skillsCircle.map((circle, index) => (
-          <div key={`circle-${index}`} className="circle-cmp app__flex">
-            <img src={circle} alt="circle" />
-          </div>
-        ))}
-      </motion.div>
+      <div className="app__header-content">
+        <Container p={0} maxW="container.md">
+          <Text fontSize="4xl">Hello, my name is</Text>
+          <Heading fontSize="6xl" padding="0">Zuhed Shaikh</Heading>
+          <HStack>
+            <Text>I'am a</Text>
+            <Typewriter 
+            onInit={(typewriter) => {
+              typewriter
+              .pauseFor(1000)
+              .typeString("Full Stack Web Developer")
+              .pauseFor(1000)
+              .deleteAll()
+              .pauseFor(1000)
+              .typeString("Problem Solver")
+              .pauseFor(1000)
+              .start()
+            }}
+            options={{
+              loop: true
+            }}
+            />
+          </HStack>
+        </Container>
+      </div>
     </div>
   );
-}
+};
 
-export default AppWrap(Header, "home")
+export default AppWrap(Header, "home");
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
 import "./index.css";
 
 const DOMroot = document.getElementById("root");
 const root = ReactDOM.createRoot(DOMroot);
 
-root.render(<App />);
+root.render(
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>
+);
